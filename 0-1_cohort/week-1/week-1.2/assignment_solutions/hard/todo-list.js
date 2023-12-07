c3c45ba7ff1bae8ebc6ffbfba3fa3a265b5477cb
iamRishvanth <rishe9752@gmail.com>
2023-12-07 20:19:12 +0530

    solution to todo-list problem added

---
+++ 0-1_cohort/week-1/week-1.2/assignment_solutions/hard/todo-list.js
@@ -0,0 +1,44 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+  Once you've implemented the logic, test your code by running
+*/
+
+class Todo {
+  constructor(){
+    this.todos = []
+  }
+  add(todo){
+    this.todos.push(todo)
+  }
+  remove(index){
+    this.todos.splice(index,1)
+  }
+  update(index,updatedTodo){
+    if(index > this.todos.length - 1){
+      //pass
+    }else{
+    this.todos.splice(index,1,updatedTodo)
+    }
+  }
+  getAll(){
+    return this.todos
+  }
+  get(index){
+    if(index > this.todos.length - 1){
+      return null
+    }
+    return this.todos[index]
+  }
+  clear(){
+    this.todos.length = 0
+  }
+}
+
+module.exports = Todo;
